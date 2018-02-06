e62119560c24fab99abbf482d8c50478044a7e36
Le Ton Chanh <letonchanh@gmail.com>
2018-02-05 21:00:05 -0600

    add main method

---
+++ src/model_checker.py
@@ -0,0 +1,16 @@
+from seplogic import *
+
+class ModelChecker(object):
+    def check(self, model, f):
+        method_name = 'check_' + type(f).__name__
+        checker = getattr(self, method_name, self.generic_check)
+        return checker(model, f)
+
+    def generic_check(self, model, f):
+        raise Exception('No model checker for ' + type(f).__name__)
+
+class SHModelChecker(ModelChecker):
+    def __init__(self):
+        pass
+
+
--- src/parser.py
+++ src/parser.py
@@ -250,33 +250,3 @@ class TreeToTraces(Transformer):
 
 trace_parser = Lark(trace_grammar, start='traces',lexer='standard')
 
-defn = """
-data node { int val; node next; };
-
-pred ls(x,y,n) := emp & x=y & n=0
-    \/ (exists v, u. x->node{v, u} * ls(u,y,n-1) & n>=1);
-"""
-
-traces = """
-0xA001 -> node{val:1; next:0xA002};
-0xA002 -> node{val:2; next:0xA003};
-0xA003 -> node{val:3; next:0xA002};
-x = 0xA001;
-y = 0xA002;
-"""
-
-defn_ast = seplogic_parser.parse(defn)
-
-# print(defn_ast)
-# print(defn_ast.pretty())
-
-f = TreeToSL().transform(defn_ast)
-print(f)
-
-traces_ast = trace_parser.parse(traces)
-print(traces_ast)
-print(traces_ast.pretty())
-
-t = TreeToTraces().transform(traces_ast)
-print(';\n'.join(map(str, t)))
-
