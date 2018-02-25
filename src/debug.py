dc37debbc1fb10525337af8f66cafe88114946d4
Le Ton Chanh <letonchanh@gmail.com>
2018-02-25 11:00:30 -0600

    printing

--- src/debug.py
+++ src/debug.py
@@ -1,19 +1,13 @@
 from inspect import getframeinfo, stack
-import printer
+from printer import *
 import ntpath
 
 def path_leaf(path):
     head, tail = ntpath.split(path)
     return tail or ntpath.basename(head)
 
-def debug(message):
-    if isinstance(message, list):
-        message = printer.str_of_list(message, str)
-    if isinstance(message, tuple):
-        s = ''
-        for m in message:
-            s = s + str(m)
-        message = s
+def debug(obj):
+    message = Printer.str_of(obj)
     caller = getframeinfo(stack()[1][0])
     print "%s:%d - %s" % (path_leaf(caller.filename),
                           caller.lineno,
--- src/printer.py
+++ src/printer.py
@@ -1,2 +1,20 @@
-def str_of_list(ls, str_of):
-    return '[' + ', '.join(map(str_of, ls)) + ']'
+class Printer(object):
+    @classmethod
+    def str_of(self, obj):
+        method_name = 'str_of_' + type(obj).__name__
+        gen_str = getattr(self, method_name, str)
+        return gen_str(obj)
+
+    @classmethod
+    def str_of_list(self, obj):
+        elems = map(lambda e: self.str_of(e), obj)
+        if any(not hasattr(e, '__dict__') for e in obj):
+            sep = ',\n'
+        else:
+            sep = ', '
+        return '[' + (sep.join(elems)) + ']'
+
+    @classmethod
+    def str_of_tuple(self, obj):
+        elems = map(lambda e: self.str_of(e), list(obj))
+        return '(' + (' ,'.join(elems)) + ')'
