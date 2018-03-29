888cedd2db6f1773b47a045c9ec387df4a9e0193
zhengguolong <guolongzheng@MacBook.local>
2018-03-29 15:16:50 -0500

    fix minor bugs

--- src/run.sh
+++ src/run.sh
@@ -12,6 +12,6 @@ popd
 
 pre=$(grep -n -E  "//pre" $FILE | cut -d: -f1)
 post=$(grep -n -E  "//post" $FILE | cut -d: -f1)
-inv=$(grep -n -E  "//inv" $FILE | cut -d: -f1)
+inv=$(grep -n -E  "//loop" $FILE | cut -d: -f1)
 
 python dynsl.py -input $fname -pre $pre -post $post -inv $inv -size $SIZE -def $DEFN
